245940340cc450a42c01e8d919174340096466da
Carlos8965 <carloscarbajal8965@gmail.com>
2025-08-17 16:52:19 -0600

    :bug: Se solucionaron errores

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,12 +4,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_DOMINIO.firebaseapp.com",
-  projectId: "TU_PROJECT_ID",
-  storageBucket: "TU_BUCKET.appspot.com",
-  messagingSenderId: "TU_SENDER_ID",
-  appId: "TU_APP_ID"
+  apiKey: "AIzaSyBd4TyA1ro-y4JK5hMmBDrM2sDOtCxEJcM",
+  authDomain: "kodigo-api-2025.firebaseapp.com",
+  projectId: "kodigo-api-2025",
+  storageBucket: "kodigo-api-2025.firebasestorage.app",
+  messagingSenderId: "1043382341388",
+  appId: "1:1043382341388:web:f1349607c4a06ad77ac329"
 };
 
 const app = initializeApp(firebaseConfig);
